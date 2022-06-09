98ec66b47c0623a8438a35af40b75a281a15c101
Matheus Lima <matheus.lima@squadra.com.br>
2022-06-08 21:22:14 -0300

    init multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer'
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoryController } from '../modules/cars/useCases/listCaregories'
 
 const categoriesRoutes = Router();
+const upload = multer({
+    dest: "./tmp"
+})
 
 categoriesRoutes.post("/", (req, res) => {
     return createCategoryController.handle(req, res)
+});
+
+categoriesRoutes.post("/import", upload.single('file'), (req, res) => {
+    const { file } = req
+    console.log(file)
+    return res.send()
 });
 
 categoriesRoutes.get("/", (req, res) => {
